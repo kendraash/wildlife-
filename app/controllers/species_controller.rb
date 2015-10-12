e39debc880bbbdf3abf666a1ac98e99a8132eb52
Kendra Ash & Janice Dunn <student@epicodus.com>
2015-10-12 14:55:35 -0700

    Add CRUD for species class

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -8,4 +8,43 @@ class SpeciesController < ApplicationController
     @species = Species.find(params[:id])
     render :show
   end
+
+  def new
+    @species = Species.new
+    render :new
+  end
+
+  def create
+    @species = Species.new(species_params)
+    if @species.save
+      redirect_to species_index_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @species = Species.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @species = Species.find(params[:id])
+    if @species.update(species_params)
+      redirect_to species_index_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @species = Species.find(params[:id])
+    @species.destroy
+    redirect_to species_index_path
+  end
+
+    private
+    def species_params
+      params.require(:species).permit(:name)
+    end
 end
